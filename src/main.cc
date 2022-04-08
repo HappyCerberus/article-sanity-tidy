a35ce48e51f7f9a354fdfc4e11030f3f010ff3c8
HappyCerberus <HappyCerberus@users.noreply.github.com>
2022-04-08 11:15:21 +0000

    Apply automatic changes

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,13 @@
 #include <iostream>
 
 class my_class {
-	public:
-		void my_method() { std::cout << "Value is " << data_member << "\n"; }
-		int data_member;
+ public:
+  void my_method() { std::cout << "Value is " << data_member << "\n"; }
+  int data_member;
 };
 
-int main() { 
-	my_class x;
-	x.my_method();
-	return 0; 
+int main() {
+  my_class x;
+  x.my_method();
+  return 0;
 }
