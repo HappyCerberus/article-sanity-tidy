cbd9431f68ad73f67c12bb9b1ecaf29d3fc43007
RNDr. Simon Toth <business@simontoth.eu>
2022-04-08 14:56:08 +0200

    Example

--- src/main.cc
+++ src/main.cc
@@ -1,1 +1,19 @@
-int main() { return 0; }
+#include <iostream>
+#include <vector>
+
+struct my_struct {
+  void my_method() {}
+};
+
+int main() {
+  int x = 1;
+
+  if (x) std::cout << "Yes!\n";
+
+  if (true) std::cout << "Always printed.\n";
+
+  std::vector<int> data;
+  if (data.size() == 0) std::cout << "Is empty.\n";
+
+  return 0;
+}
